39a6fea012d639ea9f9c0d288e8e87907d3300ef
gudtae <90135960+gudtae@users.noreply.github.com>
2023-06-08 19:08:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./static'))
-app.get('/*', (req, res) => {
-    res.sendFile(path.resolve('static', 'index.html'));
-});
+app.use(express.static('./static'));
 
 app.listen(PORT, function () {
     console.log(`Server on ${PORT}!`);
