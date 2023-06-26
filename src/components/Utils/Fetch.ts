1f842938be9b8594d2ba3cb432eb7095a4aaa24b
Taisia <alleanoraanges@gmail.com>
2023-06-26 18:33:10 +0300

    рабочий fetch

--- src/components/Utils/Fetch.ts
+++ src/components/Utils/Fetch.ts
@@ -5,11 +5,15 @@ enum METHODS {
     DELETE = 'DELETE'
 }
 type Option = {
-    data?: Record<string, unknown> | string,
-    timeout?: 5000,
+    data?: any,
+    headers?: { [key: string]: string},
+    timeout?: number,
     [key: string]: any,
 }
 function queryStringify(data: { [key: string]: any }) {
+    if (!data) {
+        throw new Error('Должна быть data');
+    }
     let result = '';
     for (const key in data) {
         result += `${result ? '&' : '?'}${key}=${data[key]}`;
@@ -91,20 +95,4 @@ export class HTTPTransport {
             options.timeout
         );
     };
-}
-function fetchWithRetry(url: string, options: { [key: string]: any }) {
-    const { retries = 1, ...restOptions } = options;
-    const transport = new HTTPTransport();
-
-    return transport.request(url, restOptions)
-        .then(xhr => new Response(xhr.responseText, { status: xhr.status, statusText: xhr.statusText }))
-        .catch(error => {
-            if (retries > 1) {
-                // Повторный запрос при ошибке
-                return fetchWithRetry(url, { ...restOptions, retries: retries - 1 });
-            } else {
-                // Все попытки исчерпаны, выбрасываем ошибку
-                throw error;
-            }
-        });
 }
